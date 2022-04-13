ecfe5b9ee3f223db9ab4c45e69dd296a4607c875
DanielaCozmescu <dani_21bcn@hotmail.com>
2022-04-13 17:40:55 +0300

    Updated Homework 4

--- Homework4/src/ro/fasttrack/lab8/Homework4.java
+++ Homework4/src/ro/fasttrack/lab8/Homework4.java
@@ -1,17 +1,25 @@
 package ro.fasttrack.lab8;
 
 public class Homework4 {
+
     public static void main(String[] args) {
         Student student1 = new Student("Ana", 9);
         Student student2 = new Student("Petre", 5.7);
         Student student3 = new Student("Vasile", 7);
         Student student4 = new Student("Elena", 8.5);
         Student student5 = new Student("Andrei", 10);
+
+        printStudent(student1);
+        printStudent(student2);
+        printStudent(student3);
+        printStudent(student4);
+        printStudent(student5);
+
+        System.out.println(student1.toString());
     }
 
     public static void printStudent(Student student) {
-        System.out.println(student.getName() + " are nota " + student.getGrade());
-        System.out.println(student.toString());
+        System.out.println(student.getName() + " grade is " + student.getGrade());
     }
 
 }
